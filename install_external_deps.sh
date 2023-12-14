ecd5db1cf3311971d7369942ad195b80ee14b4bf
Michal Wilinski <m.wilinski11@gmail.com>
2023-12-14 23:01:04 +0100

    fix: add update

--- install_external_deps.sh
+++ install_external_deps.sh
@@ -3,6 +3,7 @@ cd ..
 git clone -b humble https://github.com/ros-perception/vision_opencv.git
 cd ..
 
+sudo apt-get update
 sudo apt-get install wget unzip
 
 sudo apt install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev python3-pip python3-numpy
