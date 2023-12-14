6863715a35bdaa58148f9d5616e9db28cd5adeeb
Michal Wilinski <m.wilinski11@gmail.com>
2023-12-14 23:00:41 +0100

    feat: added opencv

--- install_external_deps.sh
+++ install_external_deps.sh
@@ -3,6 +3,32 @@ cd ..
 git clone -b humble https://github.com/ros-perception/vision_opencv.git
 cd ..
 
+sudo apt-get install wget unzip
+
+sudo apt install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev python3-pip python3-numpy
+git clone https://github.com/opencv/opencv.git -b 3.4.1 --depth 1
+git clone https://github.com/opencv/opencv_contrib.git -b $VERSION --depth 1
+
+cd opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+-D CMAKE_INSTALL_PREFIX=/usr/local \
+-D OPENCV_GENERATE_PKGCONFIG=ON \
+-D BUILD_EXAMPLES=OFF \
+-D INSTALL_PYTHON_EXAMPLES=OFF \
+-D INSTALL_C_EXAMPLES=OFF \
+-D PYTHON_EXECUTABLE=$(which python2) \
+-D BUILD_opencv_python2=OFF \
+-D PYTHON3_EXECUTABLE=$(which python3) \
+-D PYTHON3_INCLUDE_DIR=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
+-D PYTHON3_PACKAGES_PATH=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
+-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules/ 
+
+make -j $(nproc)
+sudo make install
+sudo ldconfig
+
 # ceres solver
 sudo apt-get install -y cmake libgoogle-glog-dev libatlas-base-dev libsuitesparse-dev
 wget http://ceres-solver.org/ceres-solver-2.1.0.tar.gz
