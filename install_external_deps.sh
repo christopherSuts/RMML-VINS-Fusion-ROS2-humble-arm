57459e0a9ff961835b559b1682da5f4024e3af41
Michal Wilinski <m.wilinski11@gmail.com>
2023-12-15 15:11:06 +0100

    fix: new deps

--- install_external_deps.sh
+++ install_external_deps.sh
@@ -5,75 +5,27 @@ cd ..
 
 sudo apt-get update
 sudo apt-get install wget unzip
+sudo apt-get install -y cmake libgoogle-glog-dev libatlas-base-dev libsuitesparse-dev libboost-python-dev libboost-dev libboost-filesystem-dev libboost-program-options-dev ros-humble-image-transport
 
-sudo apt install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev python3-pip python3-numpy
-git clone https://github.com/opencv/opencv.git -b 4.2.0 --depth 1
-git clone https://github.com/opencv/opencv_contrib.git -b 4.2.0 --depth 1
+git clone https://github.com/opencv/opencv.git -b 4.8.0 --depth 1
+git clone https://github.com/opencv/opencv_contrib.git -b 4.8.0 --depth 1
 
 cd opencv
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
--D INSTALL_C_EXAMPLES=OFF \
--D INSTALL_PYTHON_EXAMPLES=OFF \
--D OPENCV_GENERATE_PKGCONFIG=ON \
--D BUILD_EXAMPLES=OFF \
--D OPENCV_ENABLE_NONFREE=ON \
--D WITH_IPP=OFF \
--D BUILD_TESTS=OFF \
--D BUILD_PERF_TESTS=OFF \
--D BUILD_opencv_adas=OFF \
--D BUILD_opencv_bgsegm=OFF \
--D BUILD_opencv_bioinspired=OFF \
--D BUILD_opencv_ccalib=OFF \
--D BUILD_opencv_datasets=ON \
--D BUILD_opencv_datasettools=OFF \
--D BUILD_opencv_face=OFF \
--D BUILD_opencv_latentsvm=OFF \
--D BUILD_opencv_line_descriptor=OFF \
--D BUILD_opencv_matlab=OFF \
--D BUILD_opencv_optflow=ON \
--D BUILD_opencv_reg=OFF \
--D BUILD_opencv_saliency=OFF \
--D BUILD_opencv_surface_matching=OFF \
--D BUILD_opencv_text=OFF \
--D BUILD_opencv_tracking=ON \
--D BUILD_opencv_xobjdetect=OFF \
--D BUILD_opencv_xphoto=OFF \
--D BUILD_opencv_stereo=OFF \
--D BUILD_opencv_hdf=OFF \
--D BUILD_opencv_cvv=OFF \
--D BUILD_opencv_fuzzy=OFF \
--D BUILD_opencv_dnn=OFF \
--D BUILD_opencv_dnn_objdetect=OFF \
--D BUILD_opencv_dnn_superres=OFF \
--D BUILD_opencv_dpm=OFF \
--D BUILD_opencv_quality=OFF \
--D BUILD_opencv_rapid=OFF \
--D BUILD_opencv_rgbd=OFF \
--D BUILD_opencv_sfm=OFF \
--D BUILD_opencv_shape=ON \
--D BUILD_opencv_stitching=OFF \
--D BUILD_opencv_structured_light=OFF \
--D BUILD_opencv_alphamat=OFF \
--D BUILD_opencv_aruco=OFF \
--D BUILD_opencv_phase_unwrapping=OFF \
--D BUILD_opencv_photo=OFF \
--D BUILD_opencv_gapi=OFF \
--D BUILD_opencv_video=ON \
--D BUILD_opencv_ml=ONN \
--D BUILD_opencv_python2=OFF \
--D WITH_GSTREAMER=OFF \
--D ENABLE_PRECOMPILED_HEADERS=OFF \
--D CMAKE_INSTALL_PREFIX=/usr/local \
--D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules/ \
-../
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D OPENCV_GENERATE_PKGCONFIG=ON -D BUILD_EXAMPLES=OFF -D OPENCV_ENABLE_NONFREE=ON -D WITH_IPP=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_opencv_adas=OFF -D BUILD_opencv_bgsegm=OFF -D BUILD_opencv_bioinspired=OFF -D BUILD_opencv_ccalib=OFF -D BUILD_opencv_datasets=ON -D BUILD_opencv_datasettools=OFF -D BUILD_opencv_face=OFF -D BUILD_opencv_latentsvm=OFF -D BUILD_opencv_line_descriptor=OFF -D BUILD_opencv_matlab=OFF -D BUILD_opencv_optflow=ON -D BUILD_opencv_reg=OFF -D BUILD_opencv_saliency=OFF -D BUILD_opencv_surface_matching=OFF -D BUILD_opencv_text=OFF -D BUILD_opencv_tracking=ON -D BUILD_opencv_xobjdetect=OFF -D BUILD_opencv_xphoto=OFF -D BUILD_opencv_stereo=OFF -D BUILD_opencv_hdf=OFF -D BUILD_opencv_cvv=OFF -D BUILD_opencv_fuzzy=OFF -D BUILD_opencv_dnn=OFF -D BUILD_opencv_dnn_objdetect=OFF -D BUILD_opencv_dnn_superres=OFF -D BUILD_opencv_dpm=OFF -D BUILD_opencv_quality=OFF -D BUILD_opencv_rapid=OFF -D BUILD_opencv_rgbd=OFF -D BUILD_opencv_sfm=OFF -D BUILD_opencv_shape=ON -D BUILD_opencv_stitching=OFF -D BUILD_opencv_structured_light=OFF -D BUILD_opencv_alphamat=OFF -D BUILD_opencv_aruco=OFF -D BUILD_opencv_phase_unwrapping=OFF -D BUILD_opencv_photo=OFF -D BUILD_opencv_gapi=OFF -D BUILD_opencv_video=ON -D BUILD_opencv_ml=OFF -D BUILD_opencv_python2=OFF -D WITH_GSTREAMER=OFF -D ENABLE_PRECOMPILED_HEADERS=OFF -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules/ ../
 
-
-make -j $(nproc)
+make -j 1
 sudo make install
 sudo ldconfig
 
+cd .. && cd ..
+
+# eigen
+wget -O eigen-3.4.0.zip https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.zip 
+unzip eigen-3.4.0.zip 
+cd eigen-3.4.0 && mkdir build && cd build
+cmake ../ && sudo make install -j 1
 cd .. && cd ..
 
 # ceres solver
@@ -85,15 +37,10 @@ mkdir build && cd build
 cmake -DEXPORT_BUILD_DIR=ON \
         -DCMAKE_INSTALL_PREFIX=/usr/local \
         ../
-make -j $(nproc) # number of cores
-make test -j $(nproc)
-sudo make install -j $(nproc)
+make -j 1
+make test -j 1
+sudo make install -j 1
 cd .. && cd ..
 
-# eigen
-wget -O eigen-3.3.9.zip https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.zip 
-unzip eigen-3.3.9.zip
-cd eigen-3.3.9 && mkdir build && cd build
-cmake ../ && sudo make install -j $(nproc)
-cd .. && cd ..
+
 
